f832a9a353b5c84828ce6d2ef1ae23b5ff73f93c
Napo <10464485@polimi.it>
2017-04-11 15:25:05 +0200

    Initial Commit

---
+++ src/main/java/it/polimi/ingsw/ps42/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.ps42;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
