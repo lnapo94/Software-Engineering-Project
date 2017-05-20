5cc9e49c5674b72860c2a7fdbea86121b40adf38
Claudio <10453981@polimi.it>
2017-05-20 17:26:40 +0200

    aggiungo classe enum per tipo azione

---
+++ src/main/java/it/polimi/ingsw/ps42/model/enumeration/ActionType.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.ps42.model.enumeration;
+
+public enum ActionType {
+	//Type of Action, TAKE_ALL stands for take a card of whatever color
+	PRODUCE("Production Action"), YELD("Yeld Action"), MARKET("Market Action"), 
+	TAKE_GREEN("Take Green Card Action"), TAKE_YELLOW("Take Yellow Card Action"), TAKE_BLUE("Take Blue Card Action"),
+	TAKE_VIOLET("Take Violet Card Action"), TAKE_ALL("Take A Card Action");
+	
+	private final String message;
+	
+	private ActionType(String message) {
+		// TODO Auto-generated constructor stub
+		this.message=message;
+	}
+	@Override
+	public String toString(){
+		
+		return this.message;
+	}
+	
+}
