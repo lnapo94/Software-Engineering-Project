fa8c3e70bc4cfcd4c6d3e487093bd5dce5d917c7
Claudio <10453981@polimi.it>
2017-05-26 09:48:35 +0200

    Completata CouncilRequest

--- src/main/java/it/polimi/ingsw/ps42/model/request/CouncilRequest.java
+++ src/main/java/it/polimi/ingsw/ps42/model/request/CouncilRequest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.ps42.model.request;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.polimi.ingsw.ps42.model.effect.Obtain;
@@ -15,14 +16,20 @@ public class CouncilRequest {
 	public CouncilRequest(List<Obtain> possibleChoice, int quantity) {
 		this.possibleChoice=possibleChoice;
 		this.quantity=quantity;
+		choice=new ArrayList<>();
 	}
 	
 	public List<Obtain> getPossibleChoice() {
 		return possibleChoice;
 	}
 	
-	public void addChoice(int index) throws WrongChoiceException{
-		if(validChoice(index)){
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void addChoice(int index) throws WrongChoiceException {
+		//Adds a choice only if this is present in the possibleChoice ArrayList
+		if(validChoice(index) && possibleChoice.size()>quantity){
 			this.choice.add(possibleChoice.remove(index));
 		}
 		else throw new WrongChoiceException("The choice for this request is not valid");
@@ -30,11 +37,18 @@ public class CouncilRequest {
 	}
 	
 	private boolean validChoice(int choice){
-		
-		return false;
+		//Checks if the choice made is a valid index
+		return possibleChoice.size()>choice;
 	}
-	public void apply(Player player) {
+	public boolean apply(Player player) {
 		
+		if(choice.size()==quantity){
+			for (Obtain obtain : choice) {
+				obtain.enableEffect(player);
+			}
+			return true;
+		}
+		else return false;
 	}
 
 	
