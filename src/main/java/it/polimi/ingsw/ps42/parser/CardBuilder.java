e88b498bc8f381213372671a253845bf8d2d7592
Claudio <10453981@polimi.it>
2017-05-28 09:53:48 +0200

    Builder carte quasi completo

--- src/main/java/it/polimi/ingsw/ps42/parser/CardBuilder.java
+++ src/main/java/it/polimi/ingsw/ps42/parser/CardBuilder.java
@@ -12,18 +12,27 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import it.polimi.ingsw.ps42.model.Card;
+import it.polimi.ingsw.ps42.model.effect.CouncilObtain;
 import it.polimi.ingsw.ps42.model.effect.Effect;
+import it.polimi.ingsw.ps42.model.effect.ForEachObtain;
+import it.polimi.ingsw.ps42.model.effect.IncreaseAction;
+import it.polimi.ingsw.ps42.model.effect.IncreaseFamiliarsPoint;
+import it.polimi.ingsw.ps42.model.effect.IncreaseSingleFamiliar;
+import it.polimi.ingsw.ps42.model.effect.NoBonusInTower;
 import it.polimi.ingsw.ps42.model.effect.Obtain;
+import it.polimi.ingsw.ps42.model.enumeration.ActionType;
 import it.polimi.ingsw.ps42.model.enumeration.CardColor;
+import it.polimi.ingsw.ps42.model.enumeration.FamiliarColor;
 import it.polimi.ingsw.ps42.model.enumeration.Resource;
 import it.polimi.ingsw.ps42.model.resourcepacket.*;
 
 public class CardBuilder {
 	
-	Gson gson;
-	FileWriter writer;
-	BufferedWriter buffer;
-	Scanner scanner;
+	private Gson gson;
+	private FileWriter writer;
+	private BufferedWriter buffer;
+	private Scanner scanner;
+	private List<Obtain> councilConversion;
 	
 	public CardBuilder(String fileName) throws IOException {
 		
@@ -31,6 +40,8 @@ public class CardBuilder {
 		writer=new FileWriter(fileName);
 		buffer=new BufferedWriter(writer);
 		scanner=new Scanner(System.in);
+		
+		councilConversion = askCouncilConversion();
 	}
 	
 	private void initGson(Gson gson){
@@ -39,9 +50,10 @@ public class CardBuilder {
 		builder.registerTypeAdapter(Effect.class, new Serializer());
 		
 		gson=builder.create();
+		
 	}
 	
-	public void addCard(){
+	public void addCard() throws IOException {
 		
 		String name = askName();
 		String description = askDescription();
@@ -56,9 +68,18 @@ public class CardBuilder {
 		
 		Card card = new Card(name, description, color, period, level, costs, 
 				immediateEffects, requirements, permanentEffect, finalEffect );
-		addCardToFile(card);
+		
+		System.out.println(card.getDescription());
+		
+		Card card2= new Card("prova", "descrizione", CardColor.GREEN, 2, 2, null, null, null, null, null);
+		addCardToFile(card2);
 	}
 	
+	public void close() throws IOException{
+		
+		buffer.close();
+		scanner.close();
+	}
 	private String askName(){
 		
 		System.out.println("inserisci il nome della carta");
@@ -95,7 +116,7 @@ public class CardBuilder {
 		ArrayList<Packet> costs=new ArrayList<>();
 		System.out.println("Creare nuovo costo?(si/no)");
 		response = scanner.nextLine();
-		while(response.toUpperCase()!="NO"){
+		while(response.toUpperCase().equals("SI")){
 			
 			costs.add(askPacket());
 			System.out.println("Creare nuovo costo?(si/no)");
@@ -109,7 +130,7 @@ public class CardBuilder {
 		ArrayList<Packet> requirements = new ArrayList<>();
 		System.out.println("Creare nuovo requisito?(si/no)");
 		response = scanner.nextLine();
-		while(response.toUpperCase()!="NO"){
+		while(response.toUpperCase().equals("SI")){
 			
 			requirements.add(askPacket());
 			System.out.println("Creare nuovo requisito?(si/no)");
@@ -124,7 +145,7 @@ public class CardBuilder {
 		String response;
 		System.out.println("Creare nuovo effetto " +effectType+ "? (si/no)");
 		response = scanner.nextLine();
-		while(response.toUpperCase()!="NO"){
+		while(response.toUpperCase().equals("SI")){
 			
 			effects.add(askEffect());
 			System.out.println("Creare nuovo effetto " +effectType+ "?(si/no)");
@@ -146,7 +167,7 @@ public class CardBuilder {
 			response=scanner.nextLine();
 			System.out.println("stato attuale: "+packet);
 		}
-		while(response.toUpperCase()!="NO");
+		while(response.toUpperCase().equals("SI"));
 		return packet;
 	}
 	
@@ -159,7 +180,27 @@ public class CardBuilder {
 		case "OBTAIN":
 			effect = askObtain();
 			break;
-
+		case "FOR_EACH_OBTAIN":
+			effect = askForEachObtain();
+			break;
+		case "INCREASE_ACTION":
+			effect = askIncreaseAction();
+			break;
+		case "DO_ACTION":
+			effect = askDoAction();
+			break;	
+		case "COUNCIL_OBTAIN":
+			effect = askCouncilObtain();
+			break;
+		case "INCREASE_FAMILIARS":
+			effect = askIncreaseFamiliars();
+			break;
+		case "INCREASE_SINGLE_FAMILIAR":
+			effect = askIncreaseSingleFamiliar();
+			break;
+		case "NO_TOWER_BONUS":
+			effect = new NoBonusInTower();
+			break;
 		default:
 			System.out.println("tipo non valido");
 			break;
@@ -168,13 +209,129 @@ public class CardBuilder {
 		
 	}
 	
-	private Obtain askObtain(){
+	private Effect askIncreaseSingleFamiliar() {
+		String response;
+		int value;
+		FamiliarColor color;
+		System.out.println("Incremento familiare?");
+		response = scanner.nextLine();
+		value = Integer.parseInt(response);
+		System.out.println("Colore del familiare?");
+		response = scanner.nextLine();
+		color = FamiliarColor.parseInput(response);
 		
-		return null;
+		return new IncreaseSingleFamiliar(value, color);
 	}
-	private void addCardToFile(Card card){
+
+	private Effect askIncreaseFamiliars() {
+
+		String response;
+		int value;
+		System.out.println("Incremento familiare?");
+		response = scanner.nextLine();
+		value = Integer.parseInt(response);
 		
-		gson.toJson(card, buffer);
+		return new IncreaseFamiliarsPoint(value);
+	}
+
+	private Effect askCouncilObtain() {
+		String response;
+		int quantity;
+		System.out.println("Quantità?");
+		response = scanner.nextLine();
+		quantity = Integer.parseInt(response);
+		return new CouncilObtain(quantity, councilConversion);
+	}
+
+	private List<Obtain> askCouncilConversion(){
+		
+		System.out.println("--CREAZIONE CONVERSIONI PRIVILEGI DEL CONSIGLIO--");
+		List<Obtain> conversion = new ArrayList<>();
+		String response;
+		do{
+			Packet gain = askPacket();
+			conversion.add(new Obtain( null, gain));
+			System.out.println("Aggiungere altra conversione?");
+			response = scanner.nextLine();
+		}
+		while(response.toUpperCase().equals("SI"));
+		System.out.println("--FINE CREAZIONE LISTA CONVERSIONI PER PRIVILEGI DEL CONSIGLIO--");
+		return conversion;
+	}
+	private Effect askDoAction() {
+		
+		String response;
+		ActionType type;
+		int actionLevel;
+		Packet discount = null;
+		System.out.println("Tipo azione?");
+		response = scanner.nextLine();
+		type = ActionType.parseInput(response);
+		System.out.println("Valore azione?");
+		response = scanner.nextLine();
+		actionLevel = Integer.parseInt(response);
+		System.out.println("Aggiungere sconto? (si/no)");
+		response = scanner.nextLine();
+		if(response.toUpperCase().equals("SI") )
+			discount = askPacket();
+		return new IncreaseAction(type, actionLevel, discount);
+	}
+
+	private Effect askIncreaseAction() {
+		
+		String response;
+		ActionType type;
+		int value;
+		Packet discount = null;
+		System.out.println("Tipo azione?");
+		response = scanner.nextLine();
+		type = ActionType.parseInput(response);
+		System.out.println("Valore incremento?");
+		response = scanner.nextLine();
+		value = Integer.parseInt(response);
+		System.out.println("Aggiungere sconto? (si/no)");
+		response = scanner.nextLine();
+		if(response.toUpperCase().equals("SI"))
+			discount = askPacket();
+		return new IncreaseAction(type, value, discount);
+	}
+
+	private Effect askForEachObtain() {
+		
+		Packet requirements = null;
+		Packet gains = null;
+		String response;
+		System.out.println("Aggiungere requisiti? (si/no)");
+		response = scanner.nextLine();
+		if(response.toUpperCase().equals("SI"))
+			requirements=askPacket();
+		System.out.println("Aggiungere guadagni? (si/no)");
+		response = scanner.nextLine();
+		if(response.toUpperCase().equals("SI"))
+			gains=askPacket();
+		return new ForEachObtain(requirements, gains);
+	}
+
+	private Effect askObtain(){
+		Packet costs = null;
+		Packet gains = null;
+		String response;
+		System.out.println("Aggiungere costi? (si/no)");
+		response = scanner.nextLine();
+		if(response.toUpperCase().equals("SI"))
+			costs=askPacket();
+		System.out.println("Aggiungere guadagni? (si/no)");
+		response = scanner.nextLine();
+		if(response.toUpperCase().equals("SI"))
+			gains=askPacket();
+		return new Obtain(costs, gains);
+	}
+	private void addCardToFile(Card card) throws IOException{
+		
+		System.out.println(card);
+		String parse = gson.toJson(card);
+		System.out.println(parse);
+		writer.append(parse);
 	}
 	
 }
