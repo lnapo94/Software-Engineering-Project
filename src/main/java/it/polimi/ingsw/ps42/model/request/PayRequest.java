2f1cf2614df7a34c7e2a7ba8db36cc198b737c55
Luca Napoletano <10464485@polimi.it>
2017-05-28 18:22:06 +0200

    Implementate Request di Card

--- src/main/java/it/polimi/ingsw/ps42/model/request/PayRequest.java
+++ src/main/java/it/polimi/ingsw/ps42/model/request/PayRequest.java
@@ -5,15 +5,14 @@ import java.util.List;
 import it.polimi.ingsw.ps42.model.Card;
 import it.polimi.ingsw.ps42.model.Printable;
 
-public class ImmediateRequest extends CardRequest{
-
-	public ImmediateRequest(Card card, List<Integer> possibleChoiceIndex, List<Printable> possibleChoice) {
+public class PayRequest extends CardRequest{
+	
+	public PayRequest(Card card, List<Integer> possibleChoiceIndex, List<Printable> possibleChoice) {
 		super(card, possibleChoiceIndex, possibleChoice);
 	}
-
+	
 	@Override
 	public void apply() {
-		card.enableImmediateEffect(possibleChoiceIndex.get(userChoice));
+		card.payCard(userChoice);
 	}
-
 }
--- src/main/java/it/polimi/ingsw/ps42/model/request/RequestInterface.java
+++ src/main/java/it/polimi/ingsw/ps42/model/request/RequestInterface.java
@@ -1,5 +1,18 @@
 package it.polimi.ingsw.ps42.model.request;
 
+import java.util.List;
+
+import it.polimi.ingsw.ps42.model.Printable;
+
 public interface RequestInterface {
+	//Common Interface for all the requests
+	
+	//Apply the user's choice
 	public void apply();
+	
+	//Show possible choice
+	public List<Printable> showChoice();
+	
+	//Set user's choice
+	public void setChoice(int choice);
 }
