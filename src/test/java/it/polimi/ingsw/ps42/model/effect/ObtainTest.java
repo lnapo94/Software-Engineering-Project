debaceaffafdb03d3ee7482720e2480e1bb91260
Claudio <10453981@polimi.it>
2017-06-01 10:28:53 +0200

    Struttura per testing effetti

---
+++ src/test/java/it/polimi/ingsw/ps42/model/effect/ObtainTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.ps42.model.effect;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ObtainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
