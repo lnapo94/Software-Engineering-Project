86b561c8faea7135a5491ea3accba10fbc624cb2
Claudio <10453981@polimi.it>
2017-06-15 14:09:43 +0200

    Testing di tableView

--- src/test/java/it/polimi/ingsw/ps42/view/TableViewTest.java
+++ src/test/java/it/polimi/ingsw/ps42/view/TableViewTest.java
@@ -8,7 +8,14 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.polimi.ingsw.ps42.model.effect.CardBan;
+import it.polimi.ingsw.ps42.model.effect.CardCostBan;
+import it.polimi.ingsw.ps42.model.effect.ObtainBan;
+import it.polimi.ingsw.ps42.model.enumeration.CardColor;
+import it.polimi.ingsw.ps42.model.enumeration.FamiliarColor;
+import it.polimi.ingsw.ps42.model.enumeration.Resource;
 import it.polimi.ingsw.ps42.model.player.Player;
+import it.polimi.ingsw.ps42.model.resourcepacket.Unit;
 
 public class TableViewTest {
 
@@ -32,11 +39,44 @@ public class TableViewTest {
 		
 	}
 	
+	public void testDice(){
+		
+		table.setBlackDie(2);
+		table.setOrangeDie(3);
+		table.setWhiteDie(4);
+		assertEquals(2,table.getBlackDie());
+		assertEquals(3,table.getOrangeDie());
+		assertEquals(4,table.getWhiteDie());
+		
+		for (Player player : players) {
+			assertEquals(2,player.getFamiliarValue(FamiliarColor.BLACK));
+			assertEquals(3,player.getFamiliarValue(FamiliarColor.ORANGE));
+			assertEquals(4,player.getFamiliarValue(FamiliarColor.WHITE));
+
+		}
+	}
+	
+	public void testBanMethods(){
+		
+		table.addFirstBan(new CardBan(CardColor.BLUE));
+		table.addSecondBan(new ObtainBan(new Unit(Resource.MONEY, 1)));
+		table.addThirdBan(new CardCostBan(CardColor.YELLOW));
+		
+		table.setPlayerFirstBan("testPlayer1");
+		table.setPlayerFirstBan("testPlayer2");
+		table.setPlayerFirstBan("testPlayer3");
+		
+		//assertTrue(players.get(0).getPlayerID().equals(table.getPlayersWithFirstBan().get(0)));
+	}
+	
 	@Test
 	public void test() {
 
 		setup();
 		
+		testDice();
+		
+		testBanMethods();
 	
 	}
 
