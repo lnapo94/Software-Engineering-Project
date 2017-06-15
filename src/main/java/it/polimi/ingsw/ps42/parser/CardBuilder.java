7393a53836902edcaaa8f2f765b47439f7447c24
Claudio <10453981@polimi.it>
2017-06-15 14:04:45 +0200

    File carte primo periodo

--- src/main/java/it/polimi/ingsw/ps42/parser/CardBuilder.java
+++ src/main/java/it/polimi/ingsw/ps42/parser/CardBuilder.java
@@ -336,7 +336,7 @@ public class CardBuilder extends Builder {
 		CardBuilder builder;
 		Scanner scanner = new Scanner(System.in);
 		try {
-			builder = new CardBuilder("greenCardsThirdPeriod.json", "Resource//Position//CouncilPosition//CouncilConvertion.json");
+			builder = new CardBuilder("VioletCardsFirstPeriod.json", "Resource//Position//CouncilPosition//CouncilConvertion.json");
 			String response;
 			int i =0;
 			do{
