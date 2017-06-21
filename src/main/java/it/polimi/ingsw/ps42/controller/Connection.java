dbcaf2b8d1a6f365452790f0ae0b98934e7bb5bf
Claudio <10453981@polimi.it>
2017-06-21 18:43:40 +0200

    Aggiunta ServerSocket

---
+++ src/main/java/it/polimi/ingsw/ps42/controller/Connection.java
@@ -0,0 +1,80 @@
+package it.polimi.ingsw.ps42.controller;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Observable;
+
+import it.polimi.ingsw.ps42.message.Message;
+
+public class Connection extends Observable implements Runnable{
+
+	private Socket socket;
+	private ObjectInputStream reader;
+	private ObjectOutputStream writer;
+	private boolean active;
+	
+	public Connection(Socket socket) {
+			this.socket = socket;
+			this.active = true;
+	}
+
+	@Override
+	public void run() {
+
+		System.out.println("connection is tring to run");
+		try{
+			writer=new ObjectOutputStream(socket.getOutputStream());
+			writer.flush();
+			reader= new ObjectInputStream(socket.getInputStream());
+			while(isActive()){
+				Message message=(Message) reader.readObject();
+				System.out.println("nuovo msg da inoltrare al controller da: "+ message.getPlayerID());
+				setChanged();
+				notifyObservers(message);
+			}
+		
+		}
+		catch (IOException | ClassNotFoundException e) {
+			System.out.println("Error occurred in trasmission");
+		}
+		finally {
+			close();
+		}
+	}
+	
+	private boolean isActive(){
+		return active;
+	}
+	
+	private void close(){
+		
+		closeConnection();
+		System.out.println("deregistro il client ");
+	}
+	
+	private synchronized void closeConnection(){
+		
+		try {
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("error in socket close");
+		}
+		active=false;
+		
+	}
+	
+	public void send(Message message){
+		
+		try {
+			writer.writeObject(message);
+			writer.flush();
+		} catch (IOException e) {
+			System.out.println("Error in sending the new message to: "+message.getPlayerID() );
+		}
+		
+		
+	}
+	
+}
