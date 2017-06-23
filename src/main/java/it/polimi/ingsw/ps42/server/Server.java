c8094a9208ab0a3c03a26f7daca3815e5f54a88f
Luca Napoletano <10464485@polimi.it>
2017-06-23 15:04:38 +0200

    Inizio implementazione Server

--- src/main/java/it/polimi/ingsw/ps42/server/Server.java
+++ src/main/java/it/polimi/ingsw/ps42/server/Server.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.ps42.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -8,17 +10,24 @@ import it.polimi.ingsw.ps42.message.LoginMessage;
 
 public class Server extends UnicastRemoteObject implements ServerInterface{
 
-	
-	
-	protected Server() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 778583111570504358L;
+	private static final int SERVER_PORT = 5555;
+	
+	private ServerSocket serverSocket;
+	
+	
+	protected Server() throws RemoteException {
+		super();
+		try {
+			serverSocket = new ServerSocket(SERVER_PORT);
+			
+		} catch (IOException e) {
+			System.out.println("Error in creation of serverSocket");
+		}
+	}
 
 	@Override
 	public void addClient(ClientInterface client) throws RemoteException {
